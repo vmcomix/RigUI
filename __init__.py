9509748be41fa96f094b7675aa3d114ea6ff0298
Vlad <vmcomix@gmail.com>
2024-02-13 19:00:31 -0500

    initial commit

---
+++ __init__.py
@@ -0,0 +1,97 @@
+import bpy
+import sys
+import requests
+from pathlib import Path
+from importlib import reload
+from bpy.types import AddonPreferences
+from bpy.props import StringProperty
+from bpy.utils import register_class, unregister_class
+
+bl_info = {
+	'name' : "Rig UI"
+	,'author' : "Vlad Mokhov"
+	,'version' : (1, 0, 0)
+	,'blender' : (4, 0, 0)
+	,'description' : "Add on for displaying the UI panel on certain rigs."
+	,'location': "View 3D > Sidebar(N) > Item"
+	,'category': 'Rigging'
+}
+
+extra_modules = []
+
+missing_modules = []
+for module in extra_modules:
+    current_directory = Path(__file__).resolve().parent
+    if not Path(current_directory / module).exists():
+        missing_modules.append(module)
+
+if missing_modules == []:
+    pass # all good in the hood
+else:
+    for module in missing_modules:
+        file_url = f'https://raw.githubusercontent.com/vmcomix/RigUI/master/{module}'
+        destination = current_directory / module
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            with open(destination, 'wb') as file:
+                file.write(response.content)
+        elif response.status_code == 404:
+            print("Trouble connecting to github to update RigUI")
+            break
+    bpy.ops.script.reload()
+ 
+
+reload_list = [
+                'ui_panel',
+                'update',
+              ]
+
+# This makes sure to reload the modules when running "Reload Scripts"
+for module in reload_list:
+    if module in locals():
+        reload(sys.modules[__name__ + '.' + module])
+    else:
+        from . import ui_panel
+        from . import update
+
+class RigUIPreferences(AddonPreferences):
+
+    bl_idname = __name__
+
+    update: StringProperty(default="")
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        if self.update == "Update available":
+            row.operator("pose.rigui_update_addon", text="Update", icon="IMPORT").update = True
+        else:
+            row.operator("pose.rigui_update_addon", text="Check for Update", icon="QUESTION").check_update = True
+
+        row = layout.row()
+        if not self.update == "":
+            row.label(text=self.update)
+
+class_list = {
+    ui_panel.VIEW3D_PT_RigUI,
+    ui_panel.RIGIFY_OT_get_frame_range,
+    ui_panel.POSE_OT_rigify_generic_snap,
+    ui_panel.POSE_OT_rigify_generic_snap_bake,
+    ui_panel.POSE_OT_rigify_clear_keyframes,
+    ui_panel.POSE_OT_rigify_limb_ik2fk,
+    ui_panel.POSE_OT_rigify_limb_ik2fk_bake,
+    ui_panel.POSE_OT_rigify_leg_roll_ik2fk,
+    ui_panel.POSE_OT_rigify_leg_roll_ik2fk_bake,
+    ui_panel.POSE_OT_rigify_switch_parent,
+    ui_panel.POSE_OT_rigify_switch_parent_bake,
+    update.RigUIAddonUpdate,
+    RigUIPreferences,
+}
+
+def register():
+    for cls in class_list:
+        register_class(cls)
+
+def unregister():
+    for cls in class_list:
+        unregister_class(cls)
---
+++ update.py
@@ -0,0 +1,73 @@
+import bpy
+import os
+import requests
+from pathlib import Path
+from bpy.props import BoolProperty
+
+def download_file(url, destination):
+    response = requests.get(url)
+    with open(destination, 'wb') as file:
+        file.write(response.content)
+
+def download_repository_files():
+    files = []
+
+    current_directory = Path(__file__).resolve().parent
+    for file in current_directory.iterdir():
+        if file.name.endswith(".py"):
+            files.append(file.name)
+
+    for file in files:
+        file_url = f'https://raw.githubusercontent.com/vmcomix/RigUI/master/{file}'
+        destination = Path(os.path.split(__file__)[0]) / file
+        download_file(file_url, destination)
+
+def latest_commit_sha():
+    url = f'https://api.github.com/repos/vmcomix/RigUI/commits/master'
+    response = requests.get(url)
+    if response.status_code == 200:
+        commit_data = response.json()
+        latest_commit_sha = commit_data['sha']
+        return latest_commit_sha
+    else:
+            print(f'Error getting latest commit. Status code: {response.status_code}')
+            return None
+
+class RigUIAddonUpdate(bpy.types.Operator):
+    bl_idname = "pose.rigui_update_addon"
+    bl_label = "Update add-on"
+    bl_description = "Download and install new version."
+    bl_options = {"REGISTER"}
+
+    check_update: BoolProperty(default=False)
+
+    update: BoolProperty(default=False)
+
+    def execute(self, context):
+
+        current_directory = Path(__file__).resolve().parent
+
+        if self.check_update:
+            current_directory = Path(__file__).resolve().parent
+            if Path(current_directory / "hash" / latest_commit_sha()).exists():
+                context.preferences.addons["RigUI"].preferences.update = "Up to date"
+            else:
+                context.preferences.addons["RigUI"].preferences.update = "Update available"
+            self.check_update = False
+        elif self.update:
+            for file in Path(current_directory / "hash").iterdir():
+                hash = file.stem
+                break
+
+            old_hash = current_directory / "hash" / hash
+            new_hash = current_directory / "hash" /latest_commit_sha()
+
+            old_hash.rename(new_hash)
+            download_repository_files()
+
+            context.preferences.addons["RigUI"].preferences.update = "Updated!"
+            self.update = False
+            bpy.ops.script.reload()
+
+
+        return {'FINISHED'}
